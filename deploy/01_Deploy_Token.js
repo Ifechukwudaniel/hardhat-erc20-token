db4c7274cca4099ae9819f61d991b74827b01779
Angry Coding <mailidanielifechukwu@gmail.com>
2022-09-24 10:38:43 +0100

    Deploy Script

---
+++ deploy/01_Deploy_Token.js
@@ -0,0 +1,24 @@
+const { network } = require("hardhat")
+const { developmentChains, INITIAL_SUPPLY } = require("../helper-hardhat-config")
+const { verify } = require("../helper-functions")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    const args = [INITIAL_SUPPLY]
+    const ourToken = await deploy("OurToken", {
+        from: deployer,
+        args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmation || 1,
+    })
+    log(`Our Token deployed to ${ourToken.address}`)
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verify.......")
+        await verify(ourToken.address, args)
+    }
+}
+
+module.exports.tags = ["all", "token"]
